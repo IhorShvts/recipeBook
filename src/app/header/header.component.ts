2b7f5ee53cbaf74a21d5f0b73addfce502857744
Igor <i.shvets20011994@gmail.com>
2018-08-27 10:54:58 +0300

    recipeBook

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit{
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
+
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+    console.log("fetch");
+  }
+  ngOnInit() {
+    this.dataStorageService.getRecipes();
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  // Initialize Firebase
+  firebase: {
+    apiKey: "AIzaSyDWsPbKQbIlYMLvlXwf3EKm-bqESBGw72g",
+    authDomain: "recipe-book-222f7.firebaseapp.com",
+    databaseURL: "https://recipe-book-222f7.firebaseio.com",
+    projectId: "recipe-book-222f7",
+    storageBucket: "",
+    messagingSenderId: "808781793877"
+  }
+};
